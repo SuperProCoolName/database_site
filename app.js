46c6134691dbf02f21f4ba585a6c16559f3e9d55
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-30 16:29:49 +0300

    buying tickets

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ db.connect((err) => {
 db.query(
   `CREATE TABLE IF NOT EXISTS customers (
   idCustomers INT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(255) NOT NULL,
+  boughtTicketID INT NOT NULL,
+  email VARCHAR(255) NOT NULL,
   PRIMARY KEY (idCustomers)
 )`,
   (err, result) => {
@@ -94,6 +94,13 @@ db.query(
   }
 );
 
+const allCities = [];
+
+db.query(`SELECT DISTINCT city FROM stations`, (err, result) => {
+  if (err) throw err;
+  allCities.push(...result.map((result) => result.city));
+});
+
 app.get("/user", (req, res) => {
   db.query(
     `SELECT 
@@ -107,13 +114,20 @@ FROM
 JOIN
     stations AS dep_stations ON routes.departure_station = dep_stations.idStations
 JOIN
-    stations AS arr_stations ON routes.arrival_station = arr_stations.idStations;`,
+    stations AS arr_stations ON routes.arrival_station = arr_stations.idStations`,
     (err, results) => {
       if (err) {
         console.log(err);
         res.status(500).send("Error retrieving routes");
       } else {
-        res.render("user", { routes: results });
+        res.render("user", {
+          routes: results,
+          cities: allCities,
+          isFirstChecked: false,
+          isSecondChecked: false,
+          priceMin: 250,
+          priceMax: 1700,
+        });
       }
     }
   );
@@ -138,7 +152,13 @@ JOIN
         console.log(err);
         res.status(500).send("Error retrieving routes");
       } else {
-        res.render("admin", { routes: results });
+        res.render("admin", {
+          routes: results,
+          isFirstChecked: false,
+          isSecondChecked: false,
+          priceMin: 250,
+          priceMax: 1700,
+        });
       }
     }
   );
@@ -146,13 +166,16 @@ JOIN
 
 app.get("/filter_user", function (req, res) {
   // Получение параметров запроса
-  var idRoute = req.query.idRoute2Checkbox;
+  var idRoute = req.query.idRoute3Checkbox;
   var cityEski = req.query.cityEskiCheckbox;
-  var priceMin = req.query.price_min;
-  var priceMax = req.query.price_max;
+  var priceMin = req.query.minPriceInput;
+  var priceMax = req.query.maxPriceInput;
+  var departure_station_city = req.query.departure_station_city;
+  var arrival_station_city = req.query.arrival_station_city;
   var minPrice = 0; // устанавливаем минимальную цену по умолчанию
-  var maxPrice = 999999999; // устанавливаем максимальную цену по умолчанию
-
+  var maxPrice = 9999; // устанавливаем максимальную цену по умолчанию
+  let cityConditionDeparture = "";
+  let cityConditionArrival = "";
   var sql = `SELECT 
       routes.*,
       dep_stations.name AS departure_station_name,
@@ -166,14 +189,26 @@ app.get("/filter_user", function (req, res) {
     JOIN
       stations AS arr_stations ON routes.arrival_station = arr_stations.idStations
     WHERE 
-      price >= ? and price <= ?`;
+      price >= ? and price <= ?
+      ${cityConditionDeparture}${cityConditionArrival}`;
   var params = [priceMin || minPrice, priceMax || maxPrice];
-
+  if (departure_station_city && departure_station_city !== "") {
+    sql += " AND dep_stations.city = ?";
+    params.push(departure_station_city);
+  } else {
+    sql += " AND dep_stations.city IS NOT NULL";
+  }
+  if (arrival_station_city && arrival_station_city !== "") {
+    sql += " AND arr_stations.city = ?";
+    params.push(arrival_station_city);
+  } else {
+    sql += " AND arr_stations.city IS NOT NULL";
+  }
   var conditions = [];
-  if (idRoute) conditions.push("idRoutes = 2");
+  if (idRoute) conditions.push("idRoutes = 3");
   if (cityEski)
     conditions.push(
-      "dep_stations.city = 'Eski Qırım' OR arr_stations.city = 'Eski Qırım'"
+      "(dep_stations.city = 'Eski Qırım' OR arr_stations.city = 'Eski Qırım')"
     );
   // Соединяем условия с помощью оператора AND и добавляем их к SQL-запросу
   if (conditions.length > 0) {
@@ -187,18 +222,21 @@ app.get("/filter_user", function (req, res) {
     // Отправка списка квартир на клиент
     res.render("user", {
       routes: results,
+      cities: allCities,
       priceMin: priceMin,
       priceMax: priceMax,
+      isFirstChecked: idRoute,
+      isSecondChecked: cityEski,
     });
   });
 });
 
 app.get("/filter_admin", function (req, res) {
   // Получение параметров запроса
-  var idRoute = req.query.idRoute2Checkbox;
+  var idRoute = req.query.idRoute3Checkbox;
   var cityEski = req.query.cityEskiCheckbox;
-  var priceMin = req.query.price_min;
-  var priceMax = req.query.price_max;
+  var priceMin = req.query.minPriceInput;
+  var priceMax = req.query.maxPriceInput;
   var minPrice = 0; // устанавливаем минимальную цену по умолчанию
   var maxPrice = 999999999; // устанавливаем максимальную цену по умолчанию
 
@@ -219,7 +257,7 @@ app.get("/filter_admin", function (req, res) {
   var params = [priceMin || minPrice, priceMax || maxPrice];
 
   var conditions = [];
-  if (idRoute) conditions.push("idRoutes = 2");
+  if (idRoute) conditions.push("idRoutes = 3");
   if (cityEski)
     conditions.push(
       "dep_stations.city = 'Eski Qırım' OR arr_stations.city = 'Eski Qırım'"
@@ -236,9 +274,85 @@ app.get("/filter_admin", function (req, res) {
     // Отправка списка квартир на клиент
     res.render("admin", {
       routes: results,
+      isFirstChecked: idRoute,
+      isSecondChecked: cityEski,
       priceMin: priceMin,
       priceMax: priceMax,
     });
+  });
+});
+
+app.get("/buy_ticket/ticket_id_:id", function (req, res) {
+  let id = req.params.id;
+  db.query(
+    `SELECT 
+      routes.*,
+      dep_stations.name AS departure_station_name,
+      dep_stations.city AS departure_station_city,
+      arr_stations.name AS arrival_station_name,
+      arr_stations.city AS arrival_station_city
+    FROM
+      routes
+    JOIN
+      stations AS dep_stations ON routes.departure_station = dep_stations.idStations
+    JOIN
+      stations AS arr_stations ON routes.arrival_station = arr_stations.idStations
+    WHERE idRoutes = ?`,
+    [id],
+    function (err, results) {
+      if (err) throw err;
+      res.render("buy_ticket", { routes: results[0], isBought: false });
+    }
+  );
+});
+
+app.post("/buy_ticket", function (req, res) {
+  let ticket_id = req.body.ticket_id;
+  let email = req.body.email;
+
+  let sql = "SELECT MAX(idCustomers) AS maxId FROM customers";
+  db.query(sql, function (error, results, field) {
+    if (error) throw error;
+    let nextCustomerId = results[0].maxId + 1;
+    let sql =
+      "INSERT INTO customers (idCustomers, boughtTicketID, email) VALUES (?, ?, ?)";
+    let params = [nextCustomerId, ticket_id, email];
+    db.query(sql, params, function (error, results, fields) {
+      if (error) throw error;
+      let sql = `SELECT 
+    routes.idRoutes,
+    routes.departure_date,
+    routes.departure_time,
+    departure_station.city AS departure_city,
+    arrival_station.city AS arrival_city,
+    routes.price,
+    customers.email
+FROM
+    routes
+        JOIN
+    customers ON routes.idRoutes = customers.boughtTicketID
+        JOIN
+    stations AS departure_station ON routes.departure_station = departure_station.idStations
+        JOIN
+    stations AS arrival_station ON routes.arrival_station = arrival_station.idStations
+      WHERE idRoutes = ?`;
+      let params = [ticket_id];
+      db.query(sql, params, function (error, results, fields) {
+        if (error) throw error;
+        let ticket = results.length > 0 ? results[0] : null;
+        res.render("user_panel", {
+          ticket: ticket,
+          email: email,
+        });
+      });
+    });
+  });
+});
+
+app.get("/user_panel", function (req, res) {
+  res.render("user_panel", {
+    ticket: null,
+    email: null,
   });
 });
 
